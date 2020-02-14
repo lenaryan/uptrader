27af1ab7350bf342cfd2e0748bbd5ee935871811
lenaryan <35894595+lenaryan@users.noreply.github.com>
2020-02-15 00:54:30 +0300

    mobile styles and scripts

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var el = null;
-    if (window.innerWidth > 740) {
-        el = document.querySelector('.tabs');
-        M.Tabs.init(el);
-    }
-    else {
-        el = document.querySelectorAll('.collapsible');
-        M.Collapsible.init(el);
-    }
+    var el = document.querySelector('.tabs');
+    M.Tabs.init(el);
+    var elem = document.querySelector('.collapsible');
+    M.Collapsible.init(elem);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var top = document.querySelector('.top');
+    var parent = null;
+    window.onclick = function(e) {
+        parent = e.srcElement.parentElement;
+        if (parent.classList.contains('collapsible__item')) {
+            if (parent.classList.contains('active')) {
+                top.classList.add('bg-peach');
+            } else {
+                top.classList.remove('bg-peach');
+            }
+        }
+    }​
 });
